53b3458db158f4576b7614b778f87c57bf00d5f4
Steven Lu <tacticalazn@gmail.com>
2015-04-10 00:59:10 -0400

    fixing

--- app/topics/showTopic.js
+++ app/topics/showTopic.js
@@ -1,5 +1,7 @@
 app.controller('showTopic', function($scope, $routeParams, $http) {
 
+  $scope.posting = false;
+
   $http.get('/api/topics/' + $routeParams.id).success(function(data) {
     $scope.topic = data;
   })
@@ -12,10 +14,13 @@ app.controller('showTopic', function($scope, $routeParams, $http) {
   loadComments();
 
   $scope.postComment = function() {
+    $scope.posting = true;
     $http.post('/api/topics/' + $routeParams.id + '/comments', {
       text: $scope.newCommentText
     })
     .success(function(data) {
+      $scope.posting = false;
+      $scope.newCommentText = null;
       $scope.comments.push(data);
       loadComments();
     });
